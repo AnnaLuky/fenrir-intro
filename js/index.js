2e3ab134cd348788609417e2a0ef6a0edf3016e0
Anna Lukyanenko <anna@shere.me>
2023-08-08 22:07:25 -0700

    add message editor

--- js/index.js
+++ js/index.js
@@ -9,10 +9,28 @@ const skills = ["HTML", "CSS", "Java Script", "Adobe Illustrator", "Photoshop"];
 const skillsSection = document.querySelector("#skills");
 const skillsList = document.createElement("ul");
 skillsSection.appendChild(skillsList);
-for (let i=0; i<skills.length; i++){
+for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement("li");
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
-
+const messageForm = document.querySelector("form[name=\"leave_message\"]");
+messageForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+    console.log(usersName, usersEmail, usersMessage);
+    const messages = document.querySelector("#messages");
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> <span>${usersMessage}</span> `;
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.addEventListener("click", function (event) {
+        event.target.parentNode.remove();
+    });
+    newMessage.appendChild(removeButton);
+    messages.appendChild(newMessage);
+    event.target.reset();
+})
 
